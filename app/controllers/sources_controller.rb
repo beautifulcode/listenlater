d50d0dda76c8ac1affd0d7d1cc0bc150e4c107a9
Aaron Glenn <aaron@aaronglenn.ca>
2012-12-29 16:27:21 -0800

    fill in source controller a bit.

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -3,15 +3,30 @@ class SourcesController < ApplicationController
   respond_to :json, :html
 
   def recent
-   index 
+    @sources = Source.recent
+    respond_with @sources
   end
 
   def popular
-   index 
+    @sources = Source.popular
+    respond_with @sources
   end
 
   def suggested
-   index 
+    @sources = Source.suggested
+    respond_with @sources
+  end
+
+  def index
+    @sources = Source.all
+    respond_with @sources
+  end
+
+  def show
+    @source = Source.find(params[:id])
+    @sources = current_user.sources if current_user 
+    @sources ||= Source.recent
+    respond_with @source
   end
 
   def new
@@ -35,17 +50,6 @@ class SourcesController < ApplicationController
     else
       render :template => 'new'
     end
-  end
-
-  def index
-    @sources = Source.all
-    respond_with @sources
-  end
-
-  def show
-    @source = Source.find(params[:id])
-    @sources = current_user.sources || Source.recent
-    respond_with @source
   end
 
 end
