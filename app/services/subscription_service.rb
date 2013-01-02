919b5e672abc88b99f8fbc15099d5a05ad7e2ceb
Aaron Glenn <aaron@aaronglenn.ca>
2013-01-01 22:12:42 -0800

    Push receiving updates to the SubscriptionService

--- app/services/subscription_service.rb
+++ app/services/subscription_service.rb
@@ -7,12 +7,10 @@ class SubscriptionService
   end
 
   def self.receive(notification)
-    #service.on_notification do |notification|
-      #puts "The feed #{notification.feed_url} has been fetched (#{notification.http_status}: #{notification.message_status}) and will be fecthed again in #{(notification.next_fetch - Time.now)/60} minutes."
-      #notification.entries.each do |e|
-        #puts " - #{e.title} (#{e.link}) was published (#{e.published}) with #{e.unique_id} as unique id : \n #{e.summary} (#{e.chunk}/#{e.chunks})"
-      #end
-    #end
+    Rails.logger.info "The feed #{notification.feed_url} has been fetched (#{notification.http_status}: #{notification.message_status}) and will be fecthed again in #{(notification.next_fetch - Time.now)/60} minutes."
+    notification.entries.each do |e|
+      Rails.logger.info " - #{e.title} (#{e.link}) was published (#{e.published}) with #{e.unique_id} as unique id : \n #{e.summary} (#{e.chunk}/#{e.chunks})"
+    end
   end
 
   def self.unsubscribe(url)
--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,7 @@ module ListenLater
                                             } do |superfeedr|
       Superfeedr = superfeedr
       superfeedr.on_notification do |notification|
-        puts "The feed #{notification.feed_url} has been fetched (#{notification.http_status}: #{notification.message_status}) and will be fecthed again in #{(notification.next_fetch - Time.now)/60} minutes."
-        notification.entries.each do |e|
-          puts " - #{e.title} (#{e.link}) was published (#{e.published}) with #{e.unique_id} as unique id : \n #{e.summary} (#{e.chunk}/#{e.chunks})"
-        end
+        SubscriptionService.receive(notification)
       end
 
     end
