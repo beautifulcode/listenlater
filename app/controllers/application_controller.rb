535a76dced39f6713c3e0d5aed9a0026c67099cb
Aaron Glenn <aaron@aaronglenn.ca>
2014-08-10 21:54:22 -0700

    Remove mobile fu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  has_mobile_fu
+  #has_mobile_fu
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || welcome_or_home
